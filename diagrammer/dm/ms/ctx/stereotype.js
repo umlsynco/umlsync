9fd3c165632f0a3814207aa173f6c0ba231ed002
kalaidin <pavel.kalaidin@gmail.com>
2012-08-28 19:24:33 +0400

    Work towards profile diagram.
    Added stereotype element (based on class diagram),
    profile diagram menu,
    stereotype context menu.

---
+++ diagrammer/dm/ms/ctx/stereotype.js
@@ -0,0 +1,29 @@
+/**
+ *   Class: context menu for stereotype element
+ * 
+ */
+(function( $, dm, undefined ) {
+    var ctx = dm.ms.ctx;
+    ctx.stereotype = function(menuBuilder) {
+        var loadUrl = "http://localhost:8000/";//menuBuilder.loader.url;
+
+        return new dm.ms.ctx.common(menuBuilder, {id: "stereotype", uid:"stereotypeECtx"}, {
+        'Remove': {
+            click: function(element) {  // element is the jquery obj clicked on when context menu launched
+              menuBuilder.diagram.removeConnector(element.euid, undefined,undefined);
+              menuBuilder.diagram.removeConnector(undefined, element.euid, undefined);
+              menuBuilder.diagram.removeElement(element.euid);
+              //$('#' +   + '_Border').remove();
+            },
+            klass: "menu-item-1" // a custom css class for this menu item (usable for styling)
+        },
+        'Add tagged value': {
+            click: function(element){ 
+            if (element.addTaggedValue != undefined) 
+                element.addTaggedValue("tag", "value");
+            },
+            klass: "second-menu-item"
+        }
+        });
+    };
+})(jQuery, dm);
