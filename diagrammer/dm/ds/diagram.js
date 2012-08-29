73c274720720951eb11ec04ee0d31682453fb9e6
kalaidin <pavel.kalaidin@gmail.com>
2012-08-29 12:15:35 +0400

    Revert "Align Border top and left to 10px grid."
    This reverts commit fd219032b33745e934185a862d31c939f00c15d8.

--- diagrammer/dm/ds/diagram.js
+++ diagrammer/dm/ds/diagram.js
@@ -17,12 +17,12 @@ Version:
 
 var dm = (function( window, undefined ) {
 
-  var dm = {};
-  dm.base = {};
-  dm.menu = {};
-  dm.ds = {};
-  dm.cs = {};
-  dm.hs = {};
+    var dm = {};
+    dm.base = {};
+    dm.menu = {};
+    dm.ds = {};
+    dm.cs = {};
+    dm.hs = {};
 	dm.dm = {};
 
     return dm;
@@ -1148,7 +1148,7 @@ dm['dm'] = dm.dm;
             if (this.options['height']) {
                 $('#' + this.euid)
                 .css('width', this.options['width']).css('height', this.options['height']);
-                $('#' + this.euid + "_Border").css("width", this.options['width']).css("border", '1px solid #87CEEB');
+                $('#' + this.euid + "_Border").css("width", this.options['width']);
             }
 
             if (this.options['top'])
@@ -1383,18 +1383,15 @@ dm['dm'] = dm.dm;
                 this.onDragMove(ui);
                 if (this.options['droppable']) {
                     if (this.parrent != undefined) {
-                      this.parrent._dropElement(this, {position: {'left':this.start_operation.left + ui.left, 'top':this.start_operation.top + ui.top}});
+                      this.parrent._dropElement(this, {position: {'left':this.start_operation.left + ui.left, 'top':this.start_operation.top + ui.top}}); 
                     }
                 }
 
             }
+
             $.log("DSTOP: " + this.euid);
             this.options.dragStart = undefined;
             this.start_operation = undefined;
-            var p = $("#" + this.euid + "_Border").position();
-            var aligned_left = p.left - p.left % 10;
-            var aligned_top = p.top - p.top % 10;
-            $("#" + this.euid + "_Border").css({'left': aligned_left, 'top': aligned_top});
         }    
         });
 
