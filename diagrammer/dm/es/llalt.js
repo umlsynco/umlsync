92c486ed00f4278ff37b9a5eacf1882f32c031b9
EvgenyAlexeyev <evgeny.alexeyev@gmail.com>
2012-10-29 19:19:01 +0400

    1. Added titles for each type of alternative instance 2. Added the different separators and conditions for each element 3. Added resizable option for each separator

--- diagrammer/dm/es/llalt.js
+++ diagrammer/dm/es/llalt.js
@@ -35,17 +35,35 @@ dm.base.diagram("es.llalt", dm.es.element, {
 	getAlt: function() {
 	  if (this.options.title == "Loop")
 	    return 'Loop(<a class="editablefield">' + 10 + '</a>)';
+	  if (this.options.title == "Parallel")
+	    return 'par';
+	  if (this.options.title == "Option")
+	    return 'opt';
 	  return this.options.title;
+	},
+	getCond: function() {
+	  if (this.options.title == "Alt")
+	    return '<div id="us-dashed" style="width:100%;height:50%;border-bottom:1px dashed black;">[<a class="editablefield">if</a>]</div>[<a class="editablefield">else</a>]';
+
+	  if (this.options.title == "Option" || this.options.title == "Loop" || this.options.title == "Break")
+        return '[<a class="editablefield">condition</a>]';
+
+  	 if (this.options.title == "Parallel" || this.options.title == "Strict")
+        return '<div id="us-dashed" style="width:100%;height:30%;border-bottom:1px dashed black;"></div><div id="us-dashed" style="width:100%;height:30%;border-bottom:1px dashed black;"></div>';
 	},
     '_create': function() {
       // HTML for class structure creation
       var aux = (this.options.aux != undefined) ? "<a>&lt&lt" + this.options.aux + "&gt&gt</a><br><b>" : "";
       this.innerHtml = '<div id="' + this.euid + '" class="us-package">\
-            <div class="us-alt-body us-element-resizable-area"><div style="width:100%;height:50%;border-bottom:1px dashed black;">[<a class="editablefield">if</a>]</div>'+
-            '[<a class="editablefield">else</a>]</div>\
-            <div class="us-alt-tab"><b>'+this.getAlt()+'</b><img src="images/cornerb.png" style="position:absolute;bottom:-1px;right:-1px;"></div>\
-                                </div>';
+            <div class="us-alt-body us-element-resizable-area">'+
+			this.getCond() +
+            '</div><div class="us-alt-tab"><b>'+this.getAlt()+'</b><img src="images/cornerb.png" style="position:absolute;bottom:-1px;right:-1px;"></div>\
+            </div>';
+
       $("#" + this.parrent.euid).append(this.innerHtml);
+
+      $("#"+this.euid + " #us-dashed").resizable({'handles': 's-l', stop:function() {$(this).width("100%")}});
+
       this.element = $("#"  + this.euid);
     },
     '_init': function() {
