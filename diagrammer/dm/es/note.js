f38a128d3f4bc65a0881cca56cf916b93893f302
umlsynco <evgeny.alexeyev@googlemail.com>
2012-11-19 20:38:02 +0400

    Fixed stupid issue with note element initialization.

--- diagrammer/dm/es/note.js
+++ diagrammer/dm/es/note.js
@@ -12,13 +12,16 @@ dm.base.diagram("es.note", dm.es.element, {
     },
     '_create': function() {
       // HTML for class structure creation
-      this.innerHtml = '<div id="' + this.euid + '" class="us-note us-element-resizable-area grElement">\
+      this.innerHtml = '<div id="' + this.euid + '" class="us-note grElement">\
                         <a id="name" class="editablefield Name">' + this.options['name'] + '</a>\
                         <img src="./images/corner.png" class="us-note-corner">\
     </div>';
       $("#" + this['parrent'].euid).append(this.innerHtml);
       this.element = $("#"  + this.euid);
-    }
+    },
+	'_init': function() {
+	  this._setOptions(this.options);
+	}
 });
 //@aspect
 })(jQuery, dm);
