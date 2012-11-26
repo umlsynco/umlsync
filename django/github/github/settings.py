5d43f2dba7dd568d0d6fe7be6a77fee297085997
kalaidin <pavel.kalaidin@gmail.com>
2012-11-26 13:28:25 +0400

    Create 3pp folder for django, export and use static files.

--- django/github/github/settings.py
+++ django/github/github/settings.py
@@ -68,6 +68,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     "/home/pavel/dev/umlsync/django/github/editor",
+    "/home/pavel/dev/umlsync/django/3pp"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
