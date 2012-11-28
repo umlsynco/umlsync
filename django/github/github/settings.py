cec667da9cee6d6935c8bce1a6a36e4b6ee840b8
kalaidin <pavel.kalaidin@gmail.com>
2012-11-28 14:01:13 +0400

    Add diagrammer to STATICFILES_DIRS.

--- django/github/github/settings.py
+++ django/github/github/settings.py
@@ -68,7 +68,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     "/home/pavel/dev/umlsync/django/github/editor",
-    "/home/pavel/dev/umlsync/django/3pp"
+    "/home/pavel/dev/umlsync/django/3pp",
+    "/home/pavel/dev/umlsync/diagrammer"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
