8801508b6d8fd2b9c31aa405c416ca22fb8bf9ed
kalaidin <pavel.kalaidin@gmail.com>
2013-01-17 23:34:42 +0400

    Updated after review.

--- tools/json-to-svg.py
+++ tools/json-to-svg.py
@@ -10,16 +10,20 @@ class SVGClass(sw.container.Group):
     """
     Represent Class element grouping several SVG primitives.
     """
-    def __init__(self, x, y, width, height):
+    def __init__(self, properties):
         sw.container.Group.__init__(self)
-        m_rect = sw.shapes.Rect(insert=(x, y),
-                                size=(width, height),
-                                fill='white', stroke='black', stroke_width=1)
+        x = properties["pageX"]
+        y = properties["pageY"]
+        width = properties["width"]
+        height = properties["height"]
+        body = sw.shapes.Rect(insert=(x, y),
+                              size=(width, height),
+                              fill='white', stroke='black', stroke_width=1)
         caption = sw.shapes.Rect(insert=(x, y),
                                  size=(width, 10),
                                  fill='white', stroke='black', stroke_width=1)
-        self.add(m_rect)
         self.add(caption)
+        self.add(body)
 
 
 class CustomJSONtoSVGConverter:
@@ -30,16 +34,13 @@ class CustomJSONtoSVGConverter:
         """Read and parse JSON file."""
         with open(filename) as f:
             self.json_data = json.load(f)
-            pprint.pprint(self.json_data)
 
     def dump(self, filename):
         """Convert JSON to SVG."""
         dwg = sw.Drawing(filename=filename, debug=True)
         for element in self.json_data["elements"]:
             if element["type"] == "class":
-                #pprint.pprint(element)
-                svg_class = SVGClass(element["pageX"], element["pageY"],
-                                     element["width"], element["height"])
+                svg_class = SVGClass(element)
                 dwg.add(svg_class)
         dwg.save()
 
