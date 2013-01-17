2e8256ef2c09b0a77e5ccc6029e78d8ddfc4c49b
kalaidin <pavel.kalaidin@gmail.com>
2013-01-17 19:12:57 +0400

    Introduce SVGClass.

--- tools/json-to-svg.py
+++ tools/json-to-svg.py
@@ -6,6 +6,22 @@ import pprint
 import svgwrite as sw
 
 
+class SVGClass(sw.container.Group):
+    """
+    Represent Class element grouping several SVG primitives.
+    """
+    def __init__(self, x, y, width, height):
+        sw.container.Group.__init__(self)
+        m_rect = sw.shapes.Rect(insert=(x, y),
+                                size=(width, height),
+                                fill='white', stroke='black', stroke_width=1)
+        caption = sw.shapes.Rect(insert=(x, y),
+                                 size=(width, 10),
+                                 fill='white', stroke='black', stroke_width=1)
+        self.add(m_rect)
+        self.add(caption)
+
+
 class CustomJSONtoSVGConverter:
     def __init__(self):
         pass
@@ -14,19 +30,17 @@ class CustomJSONtoSVGConverter:
         """Read and parse JSON file."""
         with open(filename) as f:
             self.json_data = json.load(f)
-            #pprint.pprint(self.json_data)
+            pprint.pprint(self.json_data)
 
     def dump(self, filename):
         """Convert JSON to SVG."""
         dwg = sw.Drawing(filename=filename, debug=True)
-        shapes = dwg.add(dwg.g(id='shapes', fill='white'))
         for element in self.json_data["elements"]:
             if element["type"] == "class":
                 #pprint.pprint(element)
-                rect = dwg.rect(insert=(element["pageX"], element["pageY"]),
-                                size=(element["width"], element["height"]),
-                                fill='white', stroke='black', stroke_width=1)
-                shapes.add(rect)
+                svg_class = SVGClass(element["pageX"], element["pageY"],
+                                     element["width"], element["height"])
+                dwg.add(svg_class)
         dwg.save()
 
 
