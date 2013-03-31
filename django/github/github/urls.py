2dd17f4754571930c573da5c67d9ee2828ed7cf8
kalaidin <pavel.kalaidin@gmail.com>
2013-03-31 17:12:26 +0300

    Use credentials when user is logged. Also updated github API keys.

--- django/github/github/urls.py
+++ django/github/github/urls.py
@@ -22,4 +22,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
 ) + static(settings.IMAGES_URL, document_root=settings.IMAGES_ROOT)
